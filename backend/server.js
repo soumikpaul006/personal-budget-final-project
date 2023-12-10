de00bf8c2269854e3b2f3887cc1c1d79357e609a
soumikpaul006 <prick5036@gmail.com>
2023-12-10 16:46:59 -0500

    url update

--- backend/server.js
+++ backend/server.js
@@ -7,18 +7,16 @@ const port=3000
 
 const app=express()
 
-app.use(cors({
-    origin:['http://localhost:4200'],
-    credentials:true
-}))
+app.use(cors())
 
 
 app.use(cookieParser())
 app.use(express.json())
 
 app.use("/api",routes)
+//
 
-mongoose.connect("mongodb://127.0.0.1:27017/personal-budget-db",{
+mongoose.connect("mongodb+srv://spaul13:3wQNK5m1ovKtTSmZ@cluster0.msmrs2r.mongodb.net/?retryWrites=true&w=majority",{
     useNewUrlParser:true,
 })
 .then(()=>{
